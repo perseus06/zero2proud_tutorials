95811ac4bcf637462406bf18b1c0ab5ea6571b9c
perseus06 <abelal927@outlook.com>
2024-11-07 17:45:41 +0000

    application state in actix-web

--- src/startup.rs
+++ src/startup.rs
@@ -1,12 +1,17 @@
 use crate::routes::{ health_check, subscribe };
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer, Result};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    connection: PgConnection
+) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| { App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection)
         })
         .listen(listener)?
         .run();
