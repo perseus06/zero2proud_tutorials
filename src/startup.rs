535dcd9b87097d0e706b68b0dfae238c5ad92add
perseus06 <abelal927@outlook.com>
2024-11-08 15:37:55 +0000

    Facade Pattern

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,8 @@
 use crate::routes::{ health_check, subscribe };
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer, Result};
+use actix_web::web::Data;
+use actix_web::{ web, App, HttpServer };
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -12,6 +14,7 @@ pub fn run(
 
     let server = HttpServer::new(move || { 
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
