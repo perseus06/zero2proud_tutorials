d97a69f999b3ec79eebf4ada771c25ed8d375e61
perseus06 <abelal927@outlook.com>
2024-11-08 13:09:17 +0000

    update test

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -28,7 +28,7 @@ pub async fn subscribe(
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
            println!("Failed to execute query: {}", e);
-           HttpResponse::InternalServerError.finish()
+           HttpResponse::InternalServerError().finish()
         }
     }
 }
