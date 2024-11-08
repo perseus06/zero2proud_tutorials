d13022eba1c5b1df1ad489edc784482ac8870c8f
perseus06 <abelal927@outlook.com>
2024-11-08 23:34:08 +0000

    logs for integration tests

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -11,19 +11,17 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 ///
 /// We are using `impl Subscriber` as return type to avoid having to spell out the actual
 /// type of the returned subscriber, which is indeed quite complex.
-pub fn get_subscriber(
+pub fn get_subscriber<Sink>(
     name: String,
     env_filter: String,
-    // sink: Sink,
+    sink: Sink,
 ) -> impl Subscriber + Sync + Send
-// where
-//     Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
 {
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout
-    );
-
+    let formatting_layer = BunyanFormattingLayer::new(name, sink);
     Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,6 +3,21 @@ use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
+use zero2prod::telemetry::{ get_subscriber, init_subscriber };
+use std::sync::LazyLock;
+
+// Ensure that the `tracing` stack is only initialised once using `once_cell`
+static TRACING: LazyLock<()> = LazyLock::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
 
 pub struct TestApp {
     pub address: String,
@@ -10,6 +25,9 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    // The first time `initialize` is invoked the code in `TRACING` is executed.
+    // All other invocations will instead skip execution.
+    LazyLock::force(&TRACING);
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
